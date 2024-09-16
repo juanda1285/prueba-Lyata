a435e06e1541f4db64997021e60817c78398e9f8
juanda1285 <67526249+juanda1285@users.noreply.github.com>
2024-09-16 18:48:07 -0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-gray-800 text-white py-4">
         <div class="container mx-auto text-center">
-            <p>&copy; 2024 Juan David Arias@ . Todos los derechos reservados.</p>
+            <p>&copy; 2024 @Juan David Arias . Todos los derechos reservados.</p>
         </div>
     </footer>
 </template>
